122a33279778ae2aa4ea1c27d928a0788740d932
Ugne <sil.ugne@gmail.com>
2023-06-21 09:25:41 +0200

    Deleted for tags in labels in Contact Form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -6,19 +6,19 @@
         have any questions? Write me a message!</p>
       <form name="contactForm" method="POST" data-netlify="true" netlify class="space-y-8">
         <div>
-          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
           <input type="text" name="email"
             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
             placeholder="name@flowbite.com" required>
         </div>
         <div>
-          <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
           <input type="text" id="subject"
             class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
             placeholder="Let me know how I can help you" required>
         </div>
         <div class="sm:col-span-2">
-          <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
+          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
             message</label>
           <textarea name="message" id="message" rows="6"
             class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
