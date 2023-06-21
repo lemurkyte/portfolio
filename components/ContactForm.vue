f3ce72f1ab7af0d00ada63572df3b5bcee3fcba4
Ugne <sil.ugne@gmail.com>
2023-06-21 12:12:32 +0200

    netlify cotact form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,35 +1,15 @@
 <template>
-  <section class="bg-white dark:bg-gray-900">
-    <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-
-      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Want to collaborate or
-        have any questions? Write me a message!</p>
-      <form name="contactForm" method="POST" data-netlify="true" netlify class="space-y-8">
-        <div>
-          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-          <input type="text" name="email"
-            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-            placeholder="name@flowbite.com" required>
-        </div>
-        <div>
-          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-          <input type="text" id="subject"
-            class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-            placeholder="Let me know how I can help you" required>
-        </div>
-        <div class="sm:col-span-2">
-          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
-            message</label>
-          <textarea name="message" id="message" rows="6"
-            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-            placeholder="Leave a comment..."></textarea>
-        </div>
-
-        <button type="submit" class="button">Send message</button>
-
-      </form>
-    </div>
-  </section>
+  <form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
 </template>
 
 <style scoped>
